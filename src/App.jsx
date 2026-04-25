cafc6b91e164c0a5395595689e04f7e585f38db0
Timupas <164215517+Timupas@users.noreply.github.com>
2026-04-25 10:29:56 +0200

    1st

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Board from './components/Board/Board';
+import Title from './components/Title/Title';
+
+function App() {
+  return (
+    <Board>
+      <Title text="hello wrld"/>
+    </Board>
+  )
+}
+
+export default App
